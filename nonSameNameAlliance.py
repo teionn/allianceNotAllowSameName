a3c6b22c932fb9e82757de5c25d01838cc987d90
ryunnnu <ryunnnu@gmail.com>
2019-12-13 21:50:55 +0900

    たぶんエラー起きないと思うのでtry外しました

--- nonSameNameAlliance.py
+++ nonSameNameAlliance.py
@@ -9,32 +9,20 @@ def rerename(node_uuid):
             rename_name = cmds.rename(node, "{}#".format(basename))
 
 def renameEvent():
-    try:
-        rename = {}
-        for node_uuid in cmds.ls(cmds.ls(sl=True), uuid=True):
-            rerename(node_uuid)
-
-    except Exception as e:
-        sys.stderr.write(str(e))
+    for node_uuid in cmds.ls(cmds.ls(sl=True), uuid=True):
+        rerename(node_uuid)
         
 def duplicateEvent():
-    try:
-        rename = {}
-        for node_uuid in cmds.ls(cmds.ls(sl=True, dag=True, tr=True), uuid=True):
-            rerename(node_uuid)
-
-    except Exception as e:
-        sys.stderr.write(str(e))
+    for node_uuid in cmds.ls(cmds.ls(sl=True, dag=True, tr=True), uuid=True):
+        rerename(node_uuid)
 
 def main():
-    
     try:
-        if jobIds:
+        if jobIds is not None:
             for jobId in jobIds:
                 cmds.scriptJob(kill=jobId, force=True)
-            jobIds = []
-    except:
-        jobIds = []
-    
+    except:pass
+        
+    jobIds = []
     jobIds.append(cmds.scriptJob(event=["NameChanged", renameEvent], protected=True))
     jobIds.append(cmds.scriptJob(event=["SelectionChanged", duplicateEvent], protected=True))
