a0c6704c291ad36f46da73de13754986481383f6
ryunnnu <ryunnnu@gmail.com>
2019-12-13 20:25:58 +0900

    v1.0.0

---
+++ nonSameNameAlliance.py
@@ -0,0 +1,41 @@
+from maya import cmds
+import re
+
+
+def rerename(node_uuid):
+    for node in cmds.ls(node_uuid):
+        if "|" in node:
+            basename = re.search(".+\|(.+?)(\d+)?$", node).group(1)
+            rename_name = cmds.rename(node, "{}#".format(basename))
+            rename.update({node:{"name":rename_name}})
+
+def renameEvent():
+    try:
+        rename = {}
+        for node_uuid in cmds.ls(cmds.ls(sl=True), uuid=True):
+            rerename(node_uuid)
+
+    except Exception as e:
+        sys.stderr.write(str(e))
+        
+def duplicateEvent():
+    try:
+        rename = {}
+        for node_uuid in cmds.ls(cmds.ls(sl=True, dag=True, tr=True), uuid=True):
+            rerename(node_uuid)
+
+    except Exception as e:
+        sys.stderr.write(str(e))
+
+def main():
+    
+    try:
+        if jobIds:
+            for jobId in jobIds:
+                cmds.scriptJob(kill=jobId, force=True)
+            jobIds = []
+    except:
+        jobIds = []
+    
+    jobIds.append(cmds.scriptJob(event=["NameChanged", renameEvent], protected=True))
+    jobIds.append(cmds.scriptJob(event=["SelectionChanged", duplicateEvent], protected=True))
---
+++ userSetup.py
@@ -0,0 +1,1 @@
+# import nonSameNameAlliance;nonSameNameAlliance.main()
